f6d5c4dbe07318cd7d5dae1a3c1674b9f6e9c0fd
AryanShrestha123 <aryan4845i@gmail.com>
2026-06-04 17:34:56 +0545

    Frontend: Protected Routes created

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,67 @@
+import {Navigate} from "react-router-dom";
+import {jwtDecode} from "jwt-decode";
+import {REFRESH_TOKEN, ACCESS_TOKEN} from "../constants";
+import api from "../api";
+import {useState, useEffect} from "react";
+
+function ProtectedRoute({children}) {
+    const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+    useEffect(() => {
+        auth().catch(() => {
+            setIsAuthenticated(false);
+        })
+    },[]);
+
+    const refreshToken = async () => {
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+        try{
+            const res = await api.post("/api/token/refresh/", {
+                refresh: refreshToken
+            });
+            if (res.status === 200) {
+                localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                setIsAuthenticated(true);
+            } else {
+                setIsAuthenticated(false);
+            }
+        } catch (error) {
+            setIsAuthenticated(false);
+        }
+    }
+
+    const auth = async () => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (!token) {
+            setIsAuthenticated(false);
+            return;
+        }
+        const decoded = jwtDecode(token);
+        const tokenExpiration = decoded.exp
+        const now = Data.now() / 1000
+
+        if (tokenExpiration < now) {
+            try {
+                await refreshToken();
+                setIsAuthenticated(true);
+            } catch (error) {
+                setIsAuthenticated(false);
+            }
+        } else {
+            setIsAuthenticated(true);
+        }
+    }
+
+    if (isAuthenticated === null) {
+        return (
+            <div>
+                Loading...
+            </div>
+        )
+    }
+
+    return isAuthenticated ? children : <Navigate to="/login" />
+
+}
+
+export default ProtectedRoute;
