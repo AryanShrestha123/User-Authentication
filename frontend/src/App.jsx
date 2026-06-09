4fc05e1ed90e374b0eed79b4f4ce85f6c5684e72
AryanShrestha123 <aryan4845i@gmail.com>
2026-06-09 20:28:04 +0545

    Login and Register page in=mplemented and integrated with backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,33 @@
 import react from 'react'
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/login";
+import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
 
+function Logout(){
+  localStorage.clear();
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear();
+  return <Navigate to="/register" />
+}
 
 function App() {
   return (
-    <>
-    
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>}></Route>
+        <Route path="/login"element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
+        <Route path="/register-and-logout" element={<RegisterAndLogout />}></Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
